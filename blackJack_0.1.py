39ac336c86036353212a1b6b105a49e35eeeba57
h13r0nymus <h13r0nymus88@gmail.com>
2019-11-14 16:55:06 -0600

    Update blackJack_0.1.py

--- blackJack_0.1.py
+++ blackJack_0.1.py
@@ -4,24 +4,49 @@ import os
 import random
 import time
 
+# if you do it this way then you can easily add new languages without having to type out the whole dictionary. You just need
+# to create two lists, one for the card numbers and one for the suit names, and the program will do the rest for you :)
+numerical_values = [2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10, 11]
+numerical_names = ["Two", "Three", "Four", "Five","Six", "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King", "Ace"]
+all_cards = dict(zip(numerical_names, numerical_values))
+suits = ["Clubs", "Diamonds", "Spades", "Hearts"]
+with_suits = {}
+for x in suits:
+    with_suits.update({x:all_cards})        
 
-cards = {'Clubs':{'Ace':11,'Jack': 2, 'Queen': 3, 'King':4, 'Six':6, 'Seven':7, 'Eight':8, 'Nine':9, 'Ten':10},
-        'Diamonds':{'Ace':11,'Jack': 2, 'Queen': 3, 'King':4, 'Six':6, 'Seven':7, 'Eight':8, 'Nine':9, 'Ten':10},
-        'Hearts':{'Ace':11,'Jack': 2, 'Queen': 3, 'King':4, 'Six':6, 'Seven':7, 'Eight':8, 'Nine':9, 'Ten':10},
-        'Spades':{'Ace':11,'Jack': 2, 'Queen': 3, 'King':4, 'Six':6, 'Seven':7, 'Eight':8, 'Nine':9, 'Ten':10}}
+deck = copy.deepcopy(with_suits)
 
-ru_cards = {'Крестей':{'Туз':11,'Валет': 2, 'Дама': 3, 'Король':4, 'Шестерка':6, 'Семерка':7, 'Восьмерка':8, 'Девятка':9, 'Десятка':10},
-        'Бубей':{'Туз':11,'Валет': 2, 'Дама': 3, 'Король':4, 'Шестерка':6, 'Семерка':7, 'Восьмерка':8, 'Девятка':9, 'Десятка':10},
-        'Червей':{'Туз':11,'Валет': 2, 'Дама': 3, 'Король':4, 'Шестерка':6, 'Семерка':7, 'Восьмерка':8, 'Девятка':9, 'Десятка':10},
-        'Пик':{'Туз':11,'Валет': 2, 'Дама': 3, 'Король':4, 'Шестерка':6, 'Семерка':7, 'Восьмерка':8, 'Девятка':9, 'Десятка':10}}        
+def ace_test(var):
+    if var == 11:
+        start.aces += 1
 
-deck = copy.deepcopy(cards)
+def user_victory(count):
+    print(count, 'points\n')
+    print('Player won!')
+    start.user_win += 1
+    reset()
+
+def comp_victory(comp_count):
+    print(comp_count, 'points\n')
+    print('Computer won!')
+    start.comp_win += 1
+    reset()
+    choice = "yes"
+
+def reset():
+    start.comp_count = 0
+    start.count = 0
+    start.aces=0
+    print('\nScore: ' + str(start.user_win) + ':' + str(start.comp_win))
+    start.choice = input('would you like to play again?(done or yes) ')
+
+
 
 def getRandomCard():
     suit = random.sample(list(deck),1)[0]
     card = random.sample(list(deck[suit]),1)[0]
     print(card + ' of ' + suit)
-    return int(deck[suit].pop(card))    
+    return int(deck[suit].pop(card))
 
 def welcomeScreen():
     os.system('cls')
@@ -32,84 +57,80 @@ def welcomeScreen():
     time.sleep(2)
     os.system('cls')
 
-def playerTurn():
+def playerTurn(count):
     print('Card for you')
     time.sleep(1)
-    count = 0
-    count += getRandomCard()
-    print('+' + str(count), 'points')
-    return count
-    
-def compTurn():
+    start.aces
+    var = getRandomCard()
+    ace_test(var)
+    if start.count + var > 20 and start.aces !=0:
+        start.count -= 10
+        start.aces -= 1
+    return var
+
+def compTurn(comp_count):
     print('Card for AI')
     time.sleep(1)
-    comp_count = 0
-    while comp_count < 20:
-        comp_count += getRandomCard()
-        print('+' + str(comp_count), 'points')
-        return comp_count
+    global aces
+    var = getRandomCard()
+    ace_test(var)
+    if start.comp_count + var > 20 and start.aces != 0:
+        var -= 10
+        start.aces -= 1
+    return var
+
+
 
 def start():
-    welcomeScreen()
-    user_win = 0
-    comp_win = 0
-    end = 'yes'       
-    while end == 'yes':
+    start.user_win = 0
+    start.comp_win = 0
+    start.count = 0
+    start.comp_count = 0
+    start.aces = 0
+    start.choice = "yes"
+    print('\nWell, play Black Jack\n\n')
+    while start.choice == 'yes':
         os.system('cls')
-        print('Score: ' + str(user_win) + ':' + str(comp_win))        
-        print('\nWell, play Black Jack\n\n')
-        count = 0
-        comp_count = 0
-        count += playerTurn()
-        print()
-        choice = ''
-        while choice != 'no':
-            choice = input('One more?(yes/no) ')
-            print()
-            if choice == 'yes':            
-               count +=  playerTurn()
-               if count == 21:
-                print(count, 'points\n')
-                print('Player won!')
-                user_win += 1
-                break                
-               elif count > 21:
-                print(count, 'points\n')
-                print('Player lost.(')
-                comp_win += 1
+        while start.count <= 21:
+            if start.count == 21:
+                user_victory(start.count)
+            start.count = start.count + playerTurn(start.count)
+            print('Total:' + str(start.count), 'points')
+            if start.count < 21:
+                start.choice = input('One more?(yes/no) ')
                 break
-               else:
-                print(count, 'points\n')
-        if choice == 'no':
-            while comp_count < count:
-                if comp_count >= 17:
-                    print('Dangerous. AI have to think...')
-                    time.sleep(3)
-                    if random.randint(0,1) == 1:
-                        comp_count += compTurn()
-                        print(comp_count, 'points\n')
-                    else:
-                        print('It is enough for AI')
-                        break
-                else: 
-                    comp_count += compTurn()
-                    print(comp_count, 'points\n')
-                    time.sleep(1)
+            if start.count > 21:
+                print('You busted!')
+                comp_victory(start.count)
+            else:
+                start.choice = "no"
+    if start.choice == "no":
+        while start.comp_count < start.count and start.comp_count < 21:
+            start.comp_count = start.comp_count + compTurn(start.comp_count)
+            print('+' + str(start.comp_count), 'points')
+            if start.comp_count >= 17:
+                print('Dangerous. AI have to think...')
+                time.sleep(3)
+                if random.randint(0,1) == 1:
+                    start.comp_count = start.comp_count + compTurn(start.comp_count)
+                    print(start.comp_count, 'points\n')
+                else:
+                    print('It is enough for AI')
+            else:
+                start.comp_count = start.comp_count + compTurn(start.comp_count)
+                print(start.comp_count, 'points\n')
+                time.sleep(1)
+        if start.count == 21:
+            comp_victory(start.comp_count)
+        else:
+            print('Computer busted!')
+            user_victory(start.comp_count)
+    else:
+        print ("Thank you for playing")
 
-            if count <= comp_count and comp_count < 22:
-                print('Player lost.(')
-                comp_win += 1
-                
-            elif count < 22 and count > comp_count:
-                print('Player won!')
-                user_win += 1
-            elif comp_count > 21 and count < 22:
-                print('Player won!')
-                user_win += 1
-        end = input('One more game?(yes/no) ')
-    os.system('cls')
-    print('\nFinal score: ' + str(user_win) + ':' + str(comp_win))
-    input('Press ENTER to exit') 
+    choice = input ("done to to exit or yes to play another game")
+    if input == "done":
+        print('\nFinal score: ' + str(user_win) + ':' + str(comp_win))
 
 if __name__ == '__main__':
     start()
